3904002eb9d2820447ee2f014a663debdda53d22
ejiwen <skibnewrim@gmail.com>
2021-08-05 05:59:10 -0700

    change the index page content

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      items: []
-    }
+      items: [],
+    };
   }
 
   render() {
     return (
-      <div style={{ width: '550px', margin: 'auto'}}>
-        <h1> Simple App using Github Actions </h1>
+      <div style={{ width: "550px", margin: "auto" }}>
+        <h1>
+          I can pull changement from my local to apear on may EC2 ubunto
+          instance
+        </h1>
       </div>
-    )
+    );
   }
 }
 
